e1996bdb3c68398d7173a27ff2143c9b7ef57887
Jansen <jan53n@outlook.in>
2024-03-09 10:16:27 +0530

    build

---
+++ build/cellMap.js
@@ -0,0 +1,171 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-restricted-globals */
+class CellMap {
+
+    constructor(height, width) {
+        this.width = width;
+        this.height = height;
+        this.length = width * height;
+        this.cells = new Array(this.length).fill(0);
+        this.generation = 0;
+    }
+
+    setCell(x, y) {
+        const [w, h] = [this.width, this.height];
+        let xoleft, xoright, yoabove, yobelow;
+        const cell = (y * w) + x;
+
+        if (x === 0) {
+            xoleft = w - 1;
+        } else {
+            xoleft = -1;
+        }
+
+        if (y === 0) {
+            yoabove = this.length - w;
+        } else {
+            yoabove = -w;
+        }
+
+        if (x === (w - 1)) {
+            xoright = -(w - 1);
+        } else {
+            xoright = 1;
+        }
+
+        if (y === (h - 1)) {
+            yobelow = -(this.length - w);
+        } else {
+            yobelow = w;
+        }
+
+        this.cells[cell] |= 0x01;
+        this.cells[(cell + yoabove + xoleft)] += 2;
+        this.cells[(cell + yoabove)] += 2;
+        this.cells[(cell + yoabove + xoright)] += 2;
+        this.cells[(cell + xoleft)] += 2;
+        this.cells[(cell + xoright)] += 2;
+        this.cells[(cell + yobelow + xoleft)] += 2;
+        this.cells[(cell + yobelow)] += 2;
+        this.cells[(cell + yobelow + xoright)] += 2;
+    }
+
+    clearCell(x, y) {
+        const [w, h] = [this.width, this.height];
+        let xoleft, xoright, yoabove, yobelow;
+        const cell = (y * w) + x;
+
+        if (x === 0) {
+            xoleft = w - 1;
+        } else {
+            xoleft = -1;
+        }
+
+        if (y === 0) {
+            yoabove = this.length - w;
+        } else {
+            yoabove = -w;
+        }
+
+        if (x === (w - 1)) {
+            xoright = -(w - 1);
+        } else {
+            xoright = 1;
+        }
+
+        if (y === (h - 1)) {
+            yobelow = -(this.length - w);
+        } else {
+            yobelow = w;
+        }
+
+        this.cells[cell] &= ~0x01;
+        this.cells[(cell + yoabove + xoleft)] -= 2;
+        this.cells[(cell + yoabove)] -= 2;
+        this.cells[(cell + yoabove + xoright)] -= 2;
+        this.cells[(cell + xoleft)] -= 2;
+        this.cells[(cell + xoright)] -= 2;
+        this.cells[(cell + yobelow + xoleft)] -= 2;
+        this.cells[(cell + yobelow)] -= 2;
+        this.cells[(cell + yobelow + xoright)] -= 2;
+    }
+
+    getCellState(x, y) {
+        const cell = (y * this.width) + x;
+        return this.cells[cell] & 0x01;
+    }
+
+    nextGeneration() {
+        const cells = [...this.cells];
+        const [w, h] = [this.width, this.height];
+        const drawables = [];
+        let cell = 0;
+        ++this.generation;
+
+        for (let y = 0; y < h; y++) {
+            let x = 0;
+            loop1:
+            do {
+
+                while (cells[cell] == 0) {
+                    cell++;
+                    if (++x >= w) continue loop1;
+                }
+
+                const count = cells[cell] >> 1;
+
+                if (cells[cell] & 0x01) {
+
+                    if (count != 2 && count != 3) {
+                        this.clearCell(x, y);
+                        drawables.push([x, y, false]);
+                    }
+                } else {
+                    if (count == 3) {
+                        this.setCell(x, y);
+                        drawables.push([x, y, true]);
+                    }
+                }
+
+                cell++;
+
+            } while (++x < w);
+        }
+
+        return { drawables, generation: this.generation };
+    }
+}
+
+function listenForAction(type, callback, options = {}) {
+    self.addEventListener('message', ({ data }) => {
+        if (data.type === type) {
+            callback(data);
+        }
+    }, options);
+}
+
+listenForAction('map/config', (action) => {
+
+    const { width, height } = action.payload;
+    let instance = new CellMap(width, height);
+
+    listenForAction('map/next', () => {
+        const payload = instance.nextGeneration();
+        self.postMessage({ type: 'grid/draw', payload });
+    });
+
+    listenForAction('map/clear', () => {
+        instance = new CellMap(width, height);
+        self.postMessage({ type: "map/clear/completed" });
+    });
+
+    listenForAction('grid/draw', ({ payload: { drawables } }) => {
+        for (const [x, y, on] of drawables) {
+            if (on) {
+                instance.setCell(x, y);
+            } else {
+                instance.clearCell(x, y);
+            }
+        }
+    });
+}, { once: true });
